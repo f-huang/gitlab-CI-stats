17a48a44638fd96108dc573f0646292c15ac0d51
Fanny Huang <fanny@libeo.io>
2021-10-17 23:46:50 +0200

    add: gitlab_api - add function to GET /projects by id and print results

--- src/gitlab_api.py
+++ src/gitlab_api.py
@@ -1,8 +1,17 @@
 import os
+import requests
+
 from dotenv import load_dotenv
 load_dotenv()
 
 GITLAB_ACCESS_TOKEN = os.environ.get("GITLAB_ACCESS_TOKEN")
+GITLAB_API_VERSION = "v4"
+GITLAB_REST_ENDPOINT = "https://gitlab.com/api/{version}".format(version=GITLAB_API_VERSION)
+GITLAB_REQUEST_HEADERS = {'Authorization': 'Bearer ' + GITLAB_ACCESS_TOKEN, 'Content-Type': 'multipart/form-data'}
 
 def print_token(): 
   print("GITLAB_ACCESS_TOKEN", GITLAB_ACCESS_TOKEN)
+
+def get_project_by_id(project_id: str):
+  response = requests.get("{base_url}/projects/{project_id}".format(base_url=GITLAB_REST_ENDPOINT, project_id=project_id), headers=GITLAB_REQUEST_HEADERS)
+  return response.json()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from gitlab_api import print_token
+from gitlab_api import print_token, get_project_by_id
 
 if __name__ == '__main__':
   print("Hello World !")
   print_token()
+  project = get_project_by_id(11060619)
+  print(project)
