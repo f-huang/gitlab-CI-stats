57d0c2af2aaf9ddcb958d86fff8de372be1f56ea
Fanny Huang <fanny@libeo.io>
2021-10-18 23:45:42 +0200

    chore: install autopep8

--- src/gitlab_api.py
+++ src/gitlab_api.py
@@ -6,17 +6,25 @@ load_dotenv()
 
 GITLAB_ACCESS_TOKEN = os.environ.get("GITLAB_ACCESS_TOKEN")
 GITLAB_API_VERSION = "v4"
-GITLAB_REST_ENDPOINT = "https://gitlab.com/api/{version}".format(version=GITLAB_API_VERSION)
-GITLAB_REQUEST_HEADERS = {'Authorization': 'Bearer ' + GITLAB_ACCESS_TOKEN, 'Content-Type': 'multipart/form-data'}
+GITLAB_REST_ENDPOINT = "https://gitlab.com/api/{version}".format(
+    version=GITLAB_API_VERSION)
+GITLAB_REQUEST_HEADERS = {'Authorization': 'Bearer ' +
+                          GITLAB_ACCESS_TOKEN, 'Content-Type': 'multipart/form-data'}
 
-def print_token(): 
-  print("GITLAB_ACCESS_TOKEN", GITLAB_ACCESS_TOKEN)
+
+def print_token():
+    print("GITLAB_ACCESS_TOKEN", GITLAB_ACCESS_TOKEN)
+
 
 def get_project_by_id(project_id: str):
-  response = requests.get("{base_url}/projects/{project_id}".format(base_url=GITLAB_REST_ENDPOINT, project_id=project_id), headers=GITLAB_REQUEST_HEADERS)
-  return response.json()
+    response = requests.get("{base_url}/projects/{project_id}".format(
+        base_url=GITLAB_REST_ENDPOINT, project_id=project_id), headers=GITLAB_REQUEST_HEADERS)
+    return response.json()
 
-def get_project_pipelines(project_id: str, ref: str="master"):
-  params={"order_by": "updated_at", "sort": "desc", "per_page": 100, "ref": ref}
-  response = requests.get("{base_url}/projects/{project_id}/pipelines".format(base_url=GITLAB_REST_ENDPOINT, project_id=project_id), headers=GITLAB_REQUEST_HEADERS, params=params)
-  return response.json()
+
+def get_project_pipelines(project_id: str, ref: str = "master"):
+    params = {"order_by": "created_at",
+              "sort": "desc", "per_page": 100, "ref": ref}
+    response = requests.get("{base_url}/projects/{project_id}/pipelines".format(
+        base_url=GITLAB_REST_ENDPOINT, project_id=project_id), headers=GITLAB_REQUEST_HEADERS, params=params)
+    return response.json()
--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
 import argparse
-from gitlab_api import print_token, get_project_by_id, get_project_pipelines
+from gitlab_api import get_project_pipelines
 
 
 if __name__ == '__main__':
-  print("Hello World !")
+    print("Hello World !")
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("project_id", type=int, help="GitLab's project id")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("project_id", type=int, help="GitLab's project id")
+    args = parser.parse_args()
 
-  print_token()
-  project = get_project_by_id(args.project_id)
-  print(project)
-  pipelines = get_project_pipelines(args.project_id)
-  print(pipelines)
+    pipelines = get_project_pipelines(args.project_id)
+    print(pipelines)
